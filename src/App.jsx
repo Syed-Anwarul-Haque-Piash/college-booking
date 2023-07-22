c3f9381d5f275eb313c6384cb06bba78599bad3a
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-07-22 20:15:55 +0600

    starting

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-orange-400'>This is app</h1>
+     <button className="btn btn-accent">Button</button>
+    </>
+  )
+}
+
+export default App
