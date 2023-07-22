621c434f6acf42c09230683394e056053ffa4272
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-07-22 23:56:54 +0600

    banner section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-           <h1>This is home</h1> 
+            <div className="hero min-h-screen" style={{ backgroundImage: `url('https://img.freepik.com/free-photo/portrait-female-teacher-holding-notepad-green_140725-149620.jpg')` }}>
+                <div className=""></div>
+                {/* hero-overlay bg-opacity-60 */}
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold text-black">Offering access to high-quality <br />education for everyone</h1>
+                        <p className="mb-5 text-black font-bold">Access 2500+ online courses</p>
+                       
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
