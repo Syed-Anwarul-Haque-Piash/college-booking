cab7b41d9a40df8dd83436e5c68833d29e3b4ed4
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-07-23 21:09:20 +0600

    footer

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+import Footer from '../../pages/Footer/Footer';
 //import Footer from '../Footer/Footer';
 
 const Main = () => {
@@ -9,7 +10,7 @@ const Main = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            
+            <Footer></Footer>
         </div>
     );
 };
